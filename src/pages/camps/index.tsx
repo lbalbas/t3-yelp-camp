26048336dce790f73d0f2652e30efa80eaf810ba
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2023-09-11 16:23:59 -0400

    Add search page

---
+++ src/pages/camps/index.tsx
@@ -0,0 +1,72 @@
+import { api } from "~/utils/api";
+import Link from "next/link";
+import Image from "next/image";
+import type { NextPageWithLayout } from "../_app";
+import type { ReactElement } from "react";
+import Layout from "~/components/layout";
+import LoadingBlock from "~/components/loading";
+
+const Camps: NextPageWithLayout = () => {
+  const { data, isLoading } = api.camps.getFeatured.useQuery();
+
+  return (
+    <div className="flex w-full flex-col gap-12">
+      <div className="flex flex-col gap-2 rounded-md bg-stone-50 p-10">
+        <h1 className="text-3xl font-bold">Welcome to YelpCamp!</h1>
+        <p className="text-slate-600">
+          View hand-picked campgrounds from all over the world, or add your own.
+        </p>
+        <div className="flex gap-2">
+          <div className="relative">
+            <input
+              className="rounded-md border border-slate-300 px-4 py-3 pl-8"
+              placeholder="Search for camps"
+              type="text"
+            />
+          </div>
+          <button className="rounded-md bg-black px-4 py-3 text-white">
+            Search
+          </button>
+        </div>
+        <Link className="text-slate-600 underline" href="/camps/create">
+          Or add your own campground
+        </Link>
+      </div>
+      {isLoading && <LoadingBlock size={32} />}
+      {!!data && (
+        <div className="grid grid-cols-3 grid-rows-2 gap-6">
+          {data.map((camp) => {
+            return (
+              <div
+                key={camp.id}
+                className="flex flex-col gap-2 rounded-md border border-slate-300 p-3"
+              >
+                <Image
+                  className="rounded-md"
+                  src={camp.image}
+                  width={385}
+                  height={200}
+                  alt={`Photo of ${camp.name}`}
+                />
+                <h2 className="font-bold">{camp.name}</h2>
+                <p className="text-slate-600">{camp.description}</p>
+                <Link
+                  className="w-full rounded-md border border-slate-300 py-2 text-center font-bold"
+                  href={`/camps/${camp.id}`}
+                >
+                  View Campground
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+Camps.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
+
+export default Camps;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,81 +1,101 @@
 import Link from "next/link";
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Home() {
   return (
-      <div className="flex min-h-screen w-full">
-        <div className ="lg:flex lg:justify-center w-screen lg:w-[55%]">
-          <div className="relative flex flex-col gap-10 lg:gap-24 py-10 lg:w-8/12">
-            <div className="mx-auto flex w-10/12 lg:w-full">
-              <Image
-                alt="Yelp Camp Logo"
-                src="/Logo.svg"
-                height={40}
-                width={120}
-              />
-            </div>
-            <div className="lg:hidden absolute relative w-full h-72">
-              <Image
-                alt="A camp in a hill, only visible on mobile"
-                src="/Hero Image (Tablet and Mobile).jpg"
-                fill
-                objectFit="cover"
-                objectPosition="center"
-              />
-            </div>
-            <div className="w-10/12 mx-auto lg:w-full flex-grow flex-col flex gap-6">
-              <h1 className="text-3xl md:text-5xl 2xl:text-5xl font-bold">Explore the best camps on Earth.</h1>
-              <p className="text-slate-600">YelpCamp is a curated list of the best camping spots on Earth. Unfiltered and unbiased reviews.</p>
-              <div className="flex flex-col gap-4 text-slate-600">
-                <div className="flex items-center gap-3">
-                  <Image src="/Checkmark.svg" height={24} width={24} alt="Checkmark"/>
-                  <span>Add your own camp suggestions.</span>
-                </div>
-                <div className="flex items-center gap-3">
-                  <Image src="/Checkmark.svg" height={24} width={24} alt="Checkmark"/>
-                  <span>Leave reviews and experiences.</span>
-                </div>
-                <div className="flex items-center gap-3">
-                  <Image src="/Checkmark.svg" height={24} width={24} alt="Checkmark"/>
-                  <span>See locations for all camps.</span>
-                </div>
+    <div className="flex min-h-screen w-full">
+      <div className="w-screen lg:flex lg:w-[55%] lg:justify-center">
+        <div className="relative flex flex-col gap-10 py-10 lg:w-8/12 lg:gap-24">
+          <div className="mx-auto flex w-10/12 lg:w-full">
+            <Image
+              alt="Yelp Camp Logo"
+              src="/Logo.svg"
+              height={40}
+              width={120}
+            />
+          </div>
+          <div className="absolute relative h-72 w-full lg:hidden">
+            <Image
+              alt="A camp in a hill, only visible on mobile and tablets"
+              src="/Hero Image (Tablet and Mobile).jpg"
+              fill
+              objectFit="cover"
+              objectPosition="center"
+            />
+          </div>
+          <div className="mx-auto flex w-10/12 flex-grow flex-col gap-6 lg:w-full">
+            <h1 className="text-3xl font-bold md:text-5xl 2xl:text-5xl">
+              Explore the best camps on Earth.
+            </h1>
+            <p className="text-slate-600">
+              YelpCamp is a curated list of the best camping spots on Earth.
+              Unfiltered and unbiased reviews.
+            </p>
+            <div className="flex flex-col gap-4 text-slate-600">
+              <div className="flex items-center gap-3">
+                <Image
+                  src="/Checkmark.svg"
+                  height={24}
+                  width={24}
+                  alt="Checkmark"
+                />
+                <span>Add your own camp suggestions.</span>
               </div>
-              <button className="bg-black text-white rounded-md w-44 px-2 py-4">View Campgrounds</button>
-              <div className="flex flex-col text-slate-600">
-                <span>Partnered with:</span>
-                 <div className="flex items-center">
-                   <Image
-                     src="/Airbnb.svg"
-                     height={30}
-                     width={120}
-                     alt="Airbnb"
-                     />
-                     <Image
-                     src="/Booking.svg"
-                     height={30}
-                     width={120}
-                     alt="Booking"
-                     />
-                     <Image
-                     src="/Plum Guide.svg"
-                     height={30}
-                     width={120}
-                     alt="Plum Guide"
-                     />
-                 </div>
+              <div className="flex items-center gap-3">
+                <Image
+                  src="/Checkmark.svg"
+                  height={24}
+                  width={24}
+                  alt="Checkmark"
+                />
+                <span>Leave reviews and experiences.</span>
+              </div>
+              <div className="flex items-center gap-3">
+                <Image
+                  src="/Checkmark.svg"
+                  height={24}
+                  width={24}
+                  alt="Checkmark"
+                />
+                <span>See locations for all camps.</span>
+              </div>
+            </div>
+            <Link
+              href="/camps"
+              className="flex w-44 items-center justify-center rounded-md bg-black px-2 py-4 text-white"
+            >
+              View Campgrounds
+            </Link>
+            <div className="flex flex-col text-slate-600">
+              <span>Partnered with:</span>
+              <div className="flex items-center">
+                <Image src="/Airbnb.svg" height={30} width={120} alt="Airbnb" />
+                <Image
+                  src="/Booking.svg"
+                  height={30}
+                  width={120}
+                  alt="Booking"
+                />
+                <Image
+                  src="/Plum Guide.svg"
+                  height={30}
+                  width={120}
+                  alt="Plum Guide"
+                />
               </div>
             </div>
           </div>
         </div>
-        <div className="hidden lg:block relative w-[45%]">
-          <Image
-            alt="A camp in a hill, only visible on desktops"
-            src="/Hero Image.jpg"
-            fill
-            objectFit="cover"
-            objectPosition="center"
-          />
-        </div>
       </div>
+      <div className="relative hidden w-[45%] lg:block">
+        <Image
+          alt="A camp in a hill, only visible on desktops"
+          src="/Hero Image.jpg"
+          fill
+          objectFit="cover"
+          objectPosition="center"
+        />
+      </div>
+    </div>
   );
 }
