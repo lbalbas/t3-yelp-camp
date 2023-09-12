6e6794c42fd77c65a50f17a884224bc4c1b1cf24
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2023-09-12 16:55:00 -0400

    Show rating in reviews

--- src/pages/camps/[id].tsx
+++ src/pages/camps/[id].tsx
@@ -4,76 +4,90 @@ import { api } from "~/utils/api";
 import type { NextPageWithLayout } from "../_app";
 import type { ReactElement } from "react";
 import Layout from "~/components/layout";
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 import { useUser } from "@clerk/nextjs";
-import LoadingBlock from '~/components/loading'
+import LoadingBlock from "~/components/loading";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 
 dayjs.extend(relativeTime);
 
 const CampPage: NextPageWithLayout<{ id: string }> = ({ id }) => {
-	const {data} = api.camps.getOne.useQuery({id})
+  const { data } = api.camps.getOne.useQuery({ id });
 
-	if(!data) return <div> 404 - Not found</div>
+  if (!data) return <div> 404 - Not found</div>;
 
-	return(
-		<div className="flex flex-col gap-10">
-			<div className="w-4/6 mx-auto border border-slate-300 p-8 gap-3 rounded-md flex flex-col">
-				<Image
-					className="rounded-md"
-					src={data.campground.image}
-					alt={`Photo of ${data.campground.name}`}
-					width={1080}
-					height={675}
-				/>
-				<div className="flex w-full font-bold justify-between">
-					<h1 className="text-xl">{data.campground.name}</h1>
-					<span className="text-sm">{data.campground.price}</span>
-				</div>
-				<p className="text-slate-600">{data.campground.description}</p>
-				<span className="text-slate-600 italic">{`Submitted by ${data.author.username}`}</span>
-			</div>
-			<Reviews campId={id}/>
-		</div>
-		)
-}
+  return (
+    <div className="flex flex-col gap-10">
+      <div className="mx-auto flex w-4/6 flex-col gap-3 rounded-md border border-slate-300 p-8">
+        <Image
+          className="rounded-md"
+          src={data.campground.image}
+          alt={`Photo of ${data.campground.name}`}
+          width={1080}
+          height={675}
+        />
+        <div className="flex w-full justify-between font-bold">
+          <h1 className="text-xl">{data.campground.name}</h1>
+          <span className="text-sm">{data.campground.price}</span>
+        </div>
+        <p className="text-slate-600">{data.campground.description}</p>
+        <span className="italic text-slate-600">{`Submitted by ${data.author.username}`}</span>
+      </div>
+      <Reviews campId={id} />
+    </div>
+  );
+};
 
-const Reviews = (props: {campId: string}) => {
-	const {campId} = props;
-	const { isSignedIn, user } = useUser();
-	const {data, isLoading} = api.reviews.getReviews.useQuery({campId})
+const Reviews = (props: { campId: string }) => {
+  const { campId } = props;
+  const { isSignedIn } = useUser();
+  const { data, isLoading } = api.reviews.getReviews.useQuery({ campId });
 
-	if(isLoading) return <LoadingBlock size={32} />
+  if (isLoading) return <LoadingBlock size={32} />;
 
-	return (
-		<div className="mx-auto flex flex-col gap-4 items-end p-8 w-4/6 border-slate-300 border rounded-md">
-			{(!data || data.length == 0) ? 
-				(<p className="text-center w-full">No reviews of this camp yet, be the first!</p>)
-				:
-				data.map(review=>{
-					return (
-						<div className="flex w-full flex-col gap-2 pb-2 border-b border-slate-300">
-							<div className="w-full flex justify-between">
-								<h3 className="font-bold text-lg">{review.author.username}</h3>
-								<span className="text-slate-600">{`${dayjs().to(
-            dayjs(review.review.publishedAt)
-          )}`}</span>
-							</div>
-							<p>{review.review.text}</p>
-						</div>
-						)
-				})
-			}
-			{isSignedIn && (
-				<Link className="p-4 flex items-center gap-2 rounded-md text-white bg-black" href={`/camps/leaveReview/${campId}`}>
-					<Image src="/Chat Bubble.svg" alt="Chat Bubble" height={24} width={24}/>
-					<span>Leave a Review</span>
-				</Link>)}
-		</div>
-		)
-}
+  return (
+    <div className="mx-auto flex w-4/6 flex-col items-end gap-4 rounded-md border border-slate-300 p-8">
+      {!data || data.length == 0 ? (
+        <p className="w-full text-center">
+          No reviews of this camp yet, be the first!
+        </p>
+      ) : (
+        data.map((review) => {
+          return (
+            <div key={review.review.id}className="flex w-full flex-col gap-2 border-b border-slate-300 pb-4">
+              <div className="flex w-full justify-between">
+              <div className="flex items-center gap-2">
+                <h3 className="text-lg font-bold">{review.author.username}</h3>
+                <span className="flex items-center">{Array.from({ length: review.review.rating }).map((index)=> <Image key={index} alt="Star" src="/star.svg" height={16} width={16}/>)}</span>
+              </div>
+                <span className="text-slate-600 text-sm">{`${dayjs().to(
+                  dayjs(review.review.publishedAt)
+                )}`}</span>
+              </div>
+              <p className="text-slate-600">{review.review.text}</p>
+            </div>
+          );
+        })
+      )}
+      {isSignedIn && (
+        <Link
+          className="flex items-center gap-2 rounded-md bg-black p-4 text-white"
+          href={`/camps/leaveReview/${campId}`}
+        >
+          <Image
+            src="/Chat Bubble.svg"
+            alt="Chat Bubble"
+            height={24}
+            width={24}
+          />
+          <span>Leave a Review</span>
+        </Link>
+      )}
+    </div>
+  );
+};
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = generateSSGHelper();
