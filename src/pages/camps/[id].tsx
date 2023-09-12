b2d7c55ab2adb0fa29e2a5527c3f2814a9bfeffc
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2023-09-12 13:27:44 -0400

    Add reviews, change data display

--- src/pages/camps/[id].tsx
+++ src/pages/camps/[id].tsx
@@ -5,10 +5,13 @@ import type { NextPageWithLayout } from "../_app";
 import type { ReactElement } from "react";
 import Layout from "~/components/layout";
 import Image from 'next/image';
+import Link from 'next/link';
+import { useUser } from "@clerk/nextjs";
+import LoadingBlock from '~/components/loading'
 
 const CampPage: NextPageWithLayout<{ id: string }> = ({ id }) => {
 	const {data} = api.camps.getOne.useQuery({id})
-	
+
 	if(!data) return <div> 404 - Not found</div>
 
 	return(
@@ -16,18 +19,52 @@ const CampPage: NextPageWithLayout<{ id: string }> = ({ id }) => {
 			<div className="w-4/6 mx-auto border border-slate-300 p-8 gap-3 rounded-md flex flex-col">
 				<Image
 					className="rounded-md"
-					src={data.image}
-					alt={`Photo of ${data.name}`}
+					src={data.campground.image}
+					alt={`Photo of ${data.campground.name}`}
 					width={1080}
 					height={675}
 				/>
 				<div className="flex w-full font-bold justify-between">
-					<h1 className="text-xl">{data.name}</h1>
-					<span className="text-sm">{data.price}</span>
+					<h1 className="text-xl">{data.campground.name}</h1>
+					<span className="text-sm">{data.campground.price}</span>
 				</div>
-				<p className="text-slate-600">{data.description}</p>
-				<span className="text-slate-600 italic">{`Submitted by ${data.creatorId}`}</span>
+				<p className="text-slate-600">{data.campground.description}</p>
+				<span className="text-slate-600 italic">{`Submitted by ${data.author.username}`}</span>
 			</div>
+			<Reviews campId={id}/>
+		</div>
+		)
+}
+
+const Reviews = (props: {campId: string}) => {
+	const {campId} = props;
+	const { isSignedIn, user } = useUser();
+	const {data, isLoading} = api.reviews.getReviews.useQuery({campId})
+
+	if(isLoading) return <LoadingBlock size={32} />
+
+	return (
+		<div className="mx-auto flex flex-col gap-4 items-end p-8 w-4/6 border-slate-300 border rounded-md">
+			{(!data || data.length == 0) ? 
+				(<p className="text-center w-full">No reviews of this camp yet, be the first!</p>)
+				:
+				data.map(review=>{
+					return (
+						<div className="flex w-full flex-col gap-2 pb-2 border-b border-slate-300">
+							<div className="w-full flex justify-between">
+								<h3 className="font-bold text-lg">{review.author.username}</h3>
+								<span className="text-slate-600">{}</span>
+							</div>
+							<p>{review.review.text}</p>
+						</div>
+						)
+				})
+			}
+			{isSignedIn && (
+				<Link className="p-4 flex items-center gap-2 rounded-md text-white bg-black" href={`/camps/leaveReview/${campId}`}>
+					<Image src="/Chat Bubble.svg" alt="Chat Bubble" height={24} width={24}/>
+					<span>Leave a Review</span>
+				</Link>)}
 		</div>
 		)
 }
